257b76d280e1a11fc76f6520e3a19c874db32e0f
Elena Kirillova <el-ki-ri@yandex.ru>
2025-08-06 01:07:12 +0400

    Updates

---
+++ .stylelintrc.cjs
@@ -0,0 +1,11 @@
+module.exports = {
+	extends: ['stylelint-config-standard-scss'],
+	plugins: ['stylelint-scss'],
+	rules: {
+		indentation: 'tab',
+		'no-empty-source': null,
+		'scss/at-rule-no-unknown': true,
+		'property-no-unknown': [true, { ignoreProperties: ['composes'] }],
+	},
+	ignoreFiles: ['**/*.js', '**/*.ts', '**/*.tsx'],
+};
