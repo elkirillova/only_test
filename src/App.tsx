eb2a36b6e859737a6e0413fd9c2e1f089dda5c59
Elena Kirillova <el-ki-ri@yandex.ru>
2025-08-08 21:04:19 +0400

    feat: add basic slider functionality draft

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import { Layout } from './Layout'
+import { TimeLineSlider } from './components'
 
 export default function App() {
 	return (
 		<Layout>
 			<h1>Hello!</h1>
+			<TimeLineSlider />
 		</Layout>
 	)
 }
--- src/components/index.ts
+++ src/components/index.ts
@@ -1,1 +1,2 @@
 export { Container } from './ui/Container/Container'
+export { TimeLineSlider } from './TimeLineSlider/TimeLineSlider'
